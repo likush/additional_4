328866c1233ff60ff0035b638386681ca96b1d41
lkush <l.kushnariova@yandex.ru>
2017-09-19 13:43:07 +0300

    Second implementation, fix bugs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 module.exports = function multiply(first, second) {
-    let sum = first * second;
-    return String(sum)
+    first = first.split('').reverse();
+    second = second.split('').reverse();
+    const result = [];
+
+    for (let i = 0; i < first.length; i++) {
+        for (let j = 0; j < second.length; j++) {
+            result[i + j] = result[i + j] || 0;
+            result[i + j] += first[i] * second[j];
+        }
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        if (result[i] >= 10) {
+            result[i + 1] = (result[i + 1] || 0) + parseInt(result[i] / 10);
+            result[i] %= 10;
+        }
+    }
+    return result.reverse().join('');
 };
